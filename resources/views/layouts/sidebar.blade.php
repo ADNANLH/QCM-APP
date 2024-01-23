485a0393200c2d0dc1992ed0f11b16062625cd58
ADNANLH <adnanharrak5@gmail.com>
2024-01-23 20:45:10 +0100

    fixation de navbar et template adminlte

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,17 +1,32 @@
+<!-- aside -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
     <a href="{{ route('home') }}" class="brand-link">
-        <img src="https://assets.infyom.com/logo/blue_logo_150x150.png"
-             alt="AdminLTE Logo"
+        <img src="{{ asset('img/logo-solicode.png') }}"
+             alt="Solicode Logo"
              class="brand-image img-circle elevation-3">
         <span class="brand-text font-weight-light">{{ config('app.name') }}</span>
     </a>
 
+    <!-- Sidebar -->
     <div class="sidebar">
+        <!-- Sidebar Menu -->
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                @include('layouts.menu')
+                <!-- Add icons to the links using the .nav-icon class with font-awesome or any other icon font library -->
+                <li class="nav-item">
+                    <a href="../examens/index.blade.php" class="nav-link ">
+                        <i class="nav-icon fas fa-table"></i>
+                        <p>
+                           Examens
+                        </p>
+                        
+                    </a>
+                </li>
+
             </ul>
         </nav>
+        <!-- /.sidebar-menu -->
     </div>
-
+    <!-- /.sidebar -->
 </aside>
