03b6251d6da665538f8a3adb77463a645be1a43f
lharrak-botaina <lharrak.botaina@gmail.com>
2024-01-25 02:23:32 +0100

    page index des exames

--- resources/views/examens/index.blade.php
+++ resources/views/examens/index.blade.php
@@ -1,7 +1,176 @@
 @extends('layouts.layout')
 
 @section('content')
-    <div class="container-fluid">
-        <h1 class="text-black-50">Examens page!</h1>
-    </div>
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Liste des examens</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="float-sm-right">
+                            <a href="#" class="btn btn-info">
+                                <i class="fas fa-plus"></i> Nouveau Examen
+                            </a>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="p-0 col-md-3">
+                                <div class="input-group input-group-sm">
+                                    <input type="text" name="search-input" id="search-input" class="form-control"
+                                        placeholder="Recherche">
+                                    <div class="input-group-append">
+                                        <button type="submit" class="btn btn-default">
+                                            <i class="fas fa-search"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div id="search_ajax">
+
+                            {{-- @include('Projects.table') --}}
+                            <div class="card-body table-responsive p-0">
+                                <table class="table table-striped text-nowrap table-tasks ">
+                                    <thead>
+                                        <tr>
+                                            <th>N°</th>
+                                            <th>Examen</th>
+                                            <th>Description</th>
+                                            <th>Date de début</th>
+                                            <th>Date de fin</th>
+                                            <th>Actions</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>1</td>
+                                            <td>Past simple</td>
+                                            <td>Passer cette examende Past simple avant la date de 28-03-2024</td>
+                                            <td>20-03-2024</td>
+                                            <td>28-03-2024</td>
+                                            <td>
+                                                <a href="#" class='btn btn-default btn-sm'>
+                                                    <i class="far fa-eye"></i>
+                                                </a>
+                                                <a href="#" class='btn btn-default btn-sm'>
+                                                    <i class="fa-regular fa-pen-to-square"></i>
+                                                </a>
+                                                <a href="#" class='btn btn-danger btn-sm '>
+                                                    <i class="fa-regular fa-trash-can"></i>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>2</td>
+                                            <td>Past simple</td>
+                                            <td>Passer cette examende Past simple avant la date de 28-03-2024</td>
+                                            <td>20-03-2024</td>
+                                            <td>28-03-2024</td>
+                                            <td>
+                                                <a href="#" class='btn btn-default btn-sm'>
+                                                    <i class="far fa-eye"></i>
+                                                </a>
+                                                <a href="#" class='btn btn-default btn-sm'>
+                                                    <i class="fa-regular fa-pen-to-square"></i>
+                                                </a>
+                                                <a href="#" class='btn btn-danger btn-sm '>
+                                                    <i class="fa-regular fa-trash-can"></i>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>3</td>
+                                            <td>Past simple</td>
+                                            <td>Passer cette examende Past simple avant la date de 28-03-2024</td>
+                                            <td>20-03-2024</td>
+                                            <td>28-03-2024</td>
+                                            <td>
+                                                <a href="#" class='btn btn-default btn-sm'>
+                                                    <i class="far fa-eye"></i>
+                                                </a>
+                                                <a href="#" class='btn btn-default btn-sm'>
+                                                    <i class="fa-regular fa-pen-to-square"></i>
+                                                </a>
+                                                <a href="#" class='btn btn-danger btn-sm '>
+                                                    <i class="fa-regular fa-trash-can"></i>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>4</td>
+                                            <td>Past simple</td>
+                                            <td>Passer cette examende Past simple avant la date de 28-03-2024</td>
+                                            <td>20-03-2024</td>
+                                            <td>28-03-2024</td>
+                                            <td>
+                                                <a href="#" class='btn btn-default btn-sm'>
+                                                    <i class="far fa-eye"></i>
+                                                </a>
+                                                <a href="#" class='btn btn-default btn-sm'>
+                                                    <i class="fa-regular fa-pen-to-square"></i>
+                                                </a>
+                                                <a href="#" class='btn btn-danger btn-sm '>
+                                                    <i class="fa-regular fa-trash-can"></i>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>5</td>
+                                            <td>Past simple</td>
+                                            <td>Passer cette examende Past simple avant la date de 28-03-2024</td>
+                                            <td>20-03-2024</td>
+                                            <td>28-03-2024</td>
+                                            <td>
+                                                <a href="#" class='btn btn-default btn-sm'>
+                                                    <i class="far fa-eye"></i>
+                                                </a>
+                                                <a href="#" class='btn btn-default btn-sm'>
+                                                    <i class="fa-regular fa-pen-to-square"></i>
+                                                </a>
+                                                <a href="#" class='btn btn-danger btn-sm '>
+                                                    <i class="fa-regular fa-trash-can"></i>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>6</td>
+                                            <td>Past simple</td>
+                                            <td>Passer cette examende Past simple avant la date de 28-03-2024</td>
+                                            <td>20-03-2024</td>
+                                            <td>28-03-2024</td>
+                                            <td>
+                                                <a href="#" class='btn btn-default btn-sm'>
+                                                    <i class="far fa-eye"></i>
+                                                </a>
+                                                <a href="#" class='btn btn-default btn-sm'>
+                                                    <i class="fa-regular fa-pen-to-square"></i>
+                                                </a>
+                                                <a href="#" class='btn btn-danger btn-sm '>
+                                                    <i class="fa-regular fa-trash-can"></i>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                    {{-- <input type="hidden" id='page' value="1"> --}}
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 @endsection
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -2,7 +2,8 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ route('home') }}" class="brand-link text-center">
-        <img src="{{ asset('img/logo-solicode.png') }}" alt="Solicode Logo" class="brand-image img-circle elevation-3">
+        <img src="{{ asset('img/logo-solicode.png') }}" alt="Solicode Logo" class="brand-image">
+        {{-- img-circle elevation-3 --}}
         <span class="brand-text font-weight-light">{{ config('app.name') }}</span>
     </a>
 
@@ -18,7 +19,7 @@
                         <p>
                            Examens
                         </p>
-                        
+
                     </a>
                 </li>
 
